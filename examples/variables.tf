f94cd86b2255045b7bc042b35239fd75c446b78d
Marcelo Dellacroce Mansur <mansur.ufmg@gmail.com>
2021-06-04 19:12:34 -0300

    chore: recreate inital commit

--- examples/variables.tf
+++
@@ -1,9 +0,0 @@
-# variable "grafana_url" {
-#   description = "The root URL of a Grafana server"
-#   type        = string
-# }
-
-# variable "grafana_auth" {
-#   description = "API token or basic auth username:password"
-#   type        = string
-# }
